35c0dc7b783cead9f6b45678e585df5be16a0482
Courtney Downs <courtneysdowns@gmail.com>
2021-08-02 06:35:24 -0400

    updating workout-logbook server side

--- workout-logbook/server/app.js
+++ workout-logbook/server/app.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
 let sequelize = require('./db');
 let workoutlog = require('./controllers/WorkoutLogController');
+let user = require('./controllers/UserController')
 
 sequelize.sync();
 
-app.use('/WorkoutLogController', workoutlog)
+app.use(express.json());
+
+app.use('/user', user);
+
+app.use('/workoutlog', workoutlog)
 
 app.listen(3000, function() {
     console.log('App is listening on port 3000');
--- workout-logbook/server/controllers/WorkoutLogController.js
+++ workout-logbook/server/controllers/WorkoutLogController.js
@@ -1,6 +1,32 @@
 let express = require('express');
 let router = express.Router();
+let validateSession = require('../middleware/validate-session');
+const { route } = require('./UserController');
+const WorkoutLog = require('../db').import('../models/log');
 
+/* ***************************
+ ***  CREATE WORKOUT LOG ***
+ ************************** */
+router.post('/create', validateSession, (req, res) => {
+    const logEntry = {
+        description: req.body.description,
+        definition: req.body.definition,
+        result: req.body.definition,
+        owner_id: req.body.owner_id
+    }
+    WorkoutLog.create(logEntry)
+    .then(workoutlog => res.status(200).json(workoutlog))
+    .catch(err => res.status(500).json({ error: err }))
+})
 
+/* ***************************
+ *** GET ALL LOGS FOR USER ***
+ ************************** */
+ router.get('/:username', (req, res) => {
+    let username = req.params.username
+    WorkoutLog.findAll({where: {username: username}})
+    .then(workoutlogs => res.status(200).json(workoutlogs))
+    .catch(err => res.status(500).json({ error: err }))
+});
 
 module.exports = router;
