b405d92e51cd1ef09bde46404dd43c14bedd32ef
Courtney Downs <courtneysdowns@gmail.com>
2021-08-07 10:04:48 -0400

    updating workout log and react-fundamentals

--- workout-logbook/server/app.js
+++ workout-logbook/server/app.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-let express = require('express');
+let express = require("express");
 let app = express();
-let sequelize = require('./db');
-let workoutlog = require('./controllers/WorkoutLogController');
-let user = require('./controllers/UserController')
+let sequelize = require("./db");
+let workoutlog = require("./controllers/WorkoutLogController");
+let user = require("./controllers/UserController");
 
 sequelize.sync();
 
+app.use(require("./middleware/headers"));
+
 app.use(express.json());
 
-app.use('/user', user);
+app.use("/user", user);
 
-app.use('/workoutlog', workoutlog)
+app.use("/log", workoutlog);
 
-app.listen(3000, function() {
-    console.log('App is listening on port 3000');
-})
+app.listen(3000, function () {
+  console.log("App is listening on port 3000");
+});
--- workout-logbook/server/controllers/WorkoutLogController.js
+++ workout-logbook/server/controllers/WorkoutLogController.js
@@ -1,71 +1,71 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let validateSession = require('../middleware/validate-session');
-const { route } = require('./UserController');
-const WorkoutLog = require('../db').import('../models/log');
+let validateSession = require("../middleware/validate-session");
+const { route } = require("./UserController");
+const WorkoutLog = require("../db").import("../models/log");
 
 /* ***************************
  ***  CREATE WORKOUT LOG ***
  ************************** */
-router.post('/', validateSession, (req, res) => {
-    const logEntry = {
-        description: req.body.description,
-        definition: req.body.definition,
-        result: req.body.result,
-        owner_id: req.user.id
-    }
-    WorkoutLog.create(logEntry)
-    .then(workoutlog => res.status(200).json(workoutlog))
-    .catch(err => res.status(500).json({ error: err }))
-})
+router.post("/", validateSession, (req, res) => {
+  const logEntry = {
+    description: req.body.log.description,
+    definition: req.body.log.definition,
+    result: req.body.log.result,
+    owner_id: req.user.id,
+  };
+  WorkoutLog.create(logEntry)
+    .then((workoutlog) => res.status(200).json(workoutlog))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 /* ***************************
  *** GET ALL LOGS FOR USER ***
  ************************** */
- router.get('/', validateSession, (req, res) => {
-    let userid  = req.user.id
-    WorkoutLog.findAll({
-        where: {owner_id: userid}
-    })
-    .then(workoutlogs => res.status(200).json(workoutlogs))
-    .catch(err => res.status(500).json({ error: err }))
+router.get("/", validateSession, (req, res) => {
+  let userid = req.user.id;
+  WorkoutLog.findAll({
+    where: { owner_id: userid },
+  })
+    .then((workoutlogs) => res.status(200).json(workoutlogs))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 /* ***************************
  *** GET ALL LOGS BY ID ***
  ************************** */
 
-  router.get('/:id', validateSession, (req, res) => {
-    let userid  = req.user.id
-    WorkoutLog.findAll({
-        where: {owner_id: userid}
-    })
-    .then(workoutlogs => res.status(200).json(workoutlogs))
-    .catch(err => res.status(500).json({ error: err }))
+router.get("/:id", validateSession, (req, res) => {
+  let userid = req.user.id;
+  WorkoutLog.findAll({
+    where: { owner_id: userid },
+  })
+    .then((workoutlogs) => res.status(200).json(workoutlogs))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 /* ************************************
  *** UPDATE INDIVIDUAL LOGS BY USER ***
  *********************************** */
-router.put('/:id', validateSession, (req, res)=> {
+router.put("/:id", validateSession, (req, res) => {
   const updateWorkoutLog = {
-     description: req.body.description,
-     definition: req.body.definition,
-     result: req.body.result 
+    description: req.body.log.description,
+    definition: req.body.log.definition,
+    result: req.body.log.result,
   };
-  const query = { where: { id: req.params.id, owner_id: req.user.id }} 
-  
+  const query = { where: { id: req.params.id, owner_id: req.user.id } };
+
   WorkoutLog.update(updateWorkoutLog, query)
-  .then((workoutlog) => res.status(200).json(workoutlog))
-  .catch((err) => res.status(500).json({ error: err }));
+    .then((workoutlog) => res.status(200).json(workoutlog))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 /* *************************
-*** DELETE LOG ***
-************************** */
+ *** DELETE LOG ***
+ ************************** */
 router.delete("/:id", validateSession, function (req, res) {
-    const query = { where: { id: req.params.id, owner_id: req.user.id }};
+  const query = { where: { id: req.params.id, owner_id: req.user.id } };
 
-    WorkoutLog.destroy(query)
+  WorkoutLog.destroy(query)
     .then(() => res.status(200).json({ message: "Workout Log Removed" }))
     .catch((err) => res.status(500).json({ error: err }));
 });
