02f1c1156df8c034d0995930d98fa5af1e39163f
Courtney Downs <courtneysdowns@gmail.com>
2021-08-10 10:52:11 -0400

    updating client folder

--- workout-logbook/client/src/App.jsx
+++ workout-logbook/client/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import SiteBar from "./home/Navbar";
 import Auth from "./auth/Auth";
 import WorkoutIndex from "./workouts/WorkoutIndex";
